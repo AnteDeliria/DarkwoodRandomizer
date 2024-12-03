2be78b120a426f38cdb6f3c21bfe7602462f4836
Deliria <antedeliria@gmail.com>
2024-12-03 15:13:25 +0200

    moved functions to testing class

---
+++ Patches/Testing.cs
@@ -0,0 +1,23 @@
+﻿using DarkwoodRandomizer.Plugin;
+using DarkwoodRandomizer.Pools;
+using System.Linq;
+using UnityEngine;
+
+namespace DarkwoodRandomizer.Patches
+{
+    public static class Testing
+    {
+        // Test spawn characters
+        public static void SpawnCharacter(string name)
+        {
+            Core.AddPrefab(CharacterPools.ALL_CHARACTERS[name], Player.Instance.transform.position + new Vector3(100, 0, 0), Quaternion.Euler(90f, 0f, 0f), Singleton<WorldGenerator>.Instance.gameObject, true);
+            DarkwoodRandomizerPlugin.Logger.LogInfo($"Spawning {name}");
+        }
+
+        public static void SpawnCharacter(int index)
+        {
+            Core.AddPrefab(CharacterPools.ALL_CHARACTERS.Values.ElementAt(index), Player.Instance.transform.position + new Vector3(100, 0, 0), Quaternion.Euler(90f, 0f, 0f), Singleton<WorldGenerator>.Instance.gameObject, true);
+            DarkwoodRandomizerPlugin.Logger.LogInfo($"Spawning {CharacterPools.ALL_CHARACTERS.Keys.ElementAt(index)}");
+        }
+    }
+}
