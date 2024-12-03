23dc87902c28a73b1f91d30f36f6adfbcbb94510
Deliria <antedeliria@gmail.com>
2024-12-03 15:13:25 +0200

    moved functions to testing class

--- Patches/Characters.cs
+++ Patches/Characters.cs
@@ -12,21 +12,6 @@ namespace DarkwoodRandomizer.Patches
     [HarmonyPatch]
     internal static class Characters
     {
-        // Test spawn characters
-        public static void SpawnCharacter(string name)
-        {
-            Core.AddPrefab(CharacterPools.ALL_CHARACTERS[name], Player.Instance.transform.position + new Vector3(100, 0, 0), Quaternion.Euler(90f, 0f, 0f), Singleton<WorldGenerator>.Instance.gameObject, true);
-            DarkwoodRandomizerPlugin.Logger.LogInfo($"Spawning {name}");
-        }
-
-        public static void SpawnCharacter(int index)
-        {
-            Core.AddPrefab(CharacterPools.ALL_CHARACTERS.Values.ElementAt(index), Player.Instance.transform.position + new Vector3(100, 0, 0), Quaternion.Euler(90f, 0f, 0f), Singleton<WorldGenerator>.Instance.gameObject, true);
-            DarkwoodRandomizerPlugin.Logger.LogInfo($"Spawning {CharacterPools.ALL_CHARACTERS.Keys.ElementAt(index)}");
-        }
-
-
-
         [HarmonyPatch(typeof(Character), "init")]
         [HarmonyPostfix]
         private static void ModifyCharacter(Character __instance)
